af3cf4bb39cc9e0c84f0224cebc1453aeebf5259
Douglas Chan <douglas@mediushealth.org>
2019-10-14 10:19:10 +1100

    btn

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
-var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
+var fileServer = new (nodeStatic.Server)();
+var app = http.createServer(function (req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
 
 var io = socketIO.listen(app);
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
 
   // convenience function to log server messages on the client
-  function log() {
+  function log () {
     var array = ['Message from server:'];
     array.push.apply(array, arguments);
     socket.emit('log', array);
   }
 
-  socket.on('message', function(message) {
+  socket.on('message', function (message) {
     log('Client said: ', message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit('message', message);
   });
 
-  socket.on('create or join', function(room) {
+  socket.on('create or join', function (room) {
     log('Received request to create or join room ' + room);
 
     var clientsInRoom = io.sockets.adapter.rooms[room];
@@ -49,10 +49,10 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
-  socket.on('ipaddr', function() {
+  socket.on('ipaddr', function () {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
+      ifaces[dev].forEach(function (details) {
         if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
           socket.emit('ipaddr', details.address);
         }
@@ -60,7 +60,7 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
-  socket.on('bye', function(){
+  socket.on('bye', function () {
     console.log('received bye');
   });
 
